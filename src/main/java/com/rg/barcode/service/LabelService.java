b8ac27394856e240ddf03afad2e9635e8a5e2c66
Ricardo Gaete <ricardo.gaete@gmail.com>
2022-12-15 18:18:58 -0300

    Parse code

--- src/main/java/com/rg/barcode/service/LabelService.java
+++ src/main/java/com/rg/barcode/service/LabelService.java
@@ -58,6 +58,7 @@ public class LabelService {
                 ArrayList<String> values = ((ArrayList<String>) entrySet.getValue());
                 Label label = new Label();
                 String code = values.get(1).trim().replace("\"","");
+                code = code.substring(0,code.length()-2);
                 Number price = NumberFormat.getInstance().parse(values.get(4));
                 label.setCode(code);
                 String description = values.get(3);
